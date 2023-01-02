ecc059601b6fa95e5c9b0b3aa01f58dfa605b537
wldnd2 <jiwoong000628@gmail.com>
2023-01-03 00:59:21 +0900

    1주차 Lv0 문제 해결(다른 코드 추가)

--- 1주차/python/등수매기기.py
+++ 1주차/python/등수매기기.py
@@ -4,3 +4,8 @@ def solution(score):
 
 # 3시간 동안 풀었는데 //가 아니라 /여서 testcase3, 6 통과를 못했다...
 # 평균은 /로 하는거 꼭! 기억하자
+
+# :=을 사용하여 만든 다른 코드
+def solution(score):
+    temp = sorted((avg := [sum(item)/2 for item in score]), reverse=True)
+    return [temp.index(i)+1 for i in avg]
